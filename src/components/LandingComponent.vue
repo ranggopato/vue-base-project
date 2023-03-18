6eebbef976ff9c61f908c247de9e0bd0b797ef89
ranggo pato <patoranggo@gmail.com>
2023-03-18 17:09:58 +0700

    json server

---
+++ src/components/LandingComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="landing">
+    <div>
+      <h1>Welcome To My Vue Project</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingComponent",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.landing {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+}
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar">
+    <div>
+      <img class="logo" src="../assets/logo.png" alt="logo" />
+    </div>
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "NavbarComponent",
+  props: {},
+};
+</script>
+<style scoped>
+nav {
+  margin: 0%;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  min-height: 10vh;
+  background-color: #aaa6a4;
+  align-items: center;
+}
+.logo {
+  width: 50px;
+  height: 50px;
+  margin-left: 20px;
+}
+</style>
